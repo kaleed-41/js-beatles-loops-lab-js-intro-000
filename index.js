35aab60b48f08aca709e1440075d9357f86e9b23
Daniel Williams <danielwil621@gmail.com>
2021-04-29 05:10:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var plays = [];
 function theBeatlesPlay(instruments, musicians){
   for(let i = 0; i < musicians.length; i++){
-    plays.push(musicians[i] + "plays" + instruments[i]);
+    plays.push(musicians[i] + " plays " + instruments[i]);
   }
   return plays;
 }
