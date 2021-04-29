69131cea6ae6135d8a7d8b3410544d1bf2006c2c
Daniel Williams <danielwil621@gmail.com>
2021-04-29 05:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
 var plays = [];
 function theBeatlesPlay(musicians, instruments){
   for(let i = 0; i < musicians.length; i++){
-    plays.push(musicians[i] + " plays " + instruments[i]);
+    plays.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return plays;
 }
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
+function
