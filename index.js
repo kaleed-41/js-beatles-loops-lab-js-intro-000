2ec8c7f86d5fb8af29cbcf9066a214aaa13141bf
Daniel Williams <danielwil621@gmail.com>
2021-04-29 05:19:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,3 @@ function theBeatlesPlay(musicians, instruments){
   }
   return plays;
 }
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
-
-function
