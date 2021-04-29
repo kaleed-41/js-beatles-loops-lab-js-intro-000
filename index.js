3fde503ab6ff5e04a6f6dbdb59f9391bb93f5546
Daniel Williams <danielwil621@gmail.com>
2021-04-29 05:11:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 var plays = [];
-function theBeatlesPlay(instruments, musicians){
+function theBeatlesPlay(musicians, instruments){
   for(let i = 0; i < musicians.length; i++){
     plays.push(musicians[i] + " plays " + instruments[i]);
   }
