473b68cee270c8ba593a02220ee24efca2884b8d
Daniel Williams <danielwil621@gmail.com>
2021-04-29 05:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function johnLennonFacts(facts){
   var array = [];
   var i = 0;
   while(array.length < facts.length){
-    array.push(`${facts[i]} !!!`);
+    array.push(`${facts[i]}  !!!`);
     i++;
   }
   return array;
